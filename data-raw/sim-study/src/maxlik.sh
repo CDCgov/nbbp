d3b4b54f859aa17f18d88e9e24cf98c700b6b599
afmagee42 <rzg0@cdc.gov>
2025-02-13 18:00:23 +0000

    bug

--- data-raw/sim-study/src/maxlik.sh
+++ data-raw/sim-study/src/maxlik.sh
@@ -6,11 +6,11 @@ IFS3=$'\n' read -d '' -r -a rvec < par/r.txt
 IFS4=$'\n' read -d '' -r -a kvec < par/k.txt
 
 seed=0
-offset=0
-for i in "${ivec[@]}"; do
-    for n in "${nvec[@]}"; do
-        for r in "${rvec[@]}"; do
-            for k in "${kvec[@]}"; do
+for r in "${rvec[@]}"; do
+    for k in "${kvec[@]}"; do
+        offset=0
+        for i in "${ivec[@]}"; do
+            for n in "${nvec[@]}"; do
                 timeout 30s Rscript --vanilla -e "source(\"src/run_analysis.R\"); fit_ml(${offset}, ${n}, \"data/r${r}_k${k}.txt\", \"maxlik/i${i}_n${n}_r${r}_k${k}.txt\", ${seed})"
                 seed=$((seed+1))
                 offset=$((offset+n))
